a8f0542e5a4a83aba12b5d5b80e41ee3218ae531
robert m wren <things@soul-fresh.com>
2023-11-10 18:11:25 -0500

    chore: testing TermCursor highlight group

--- nvchad/highlights.lua
+++ nvchad/highlights.lua
@@ -9,11 +9,39 @@ M.override = {
   Comment = {
     italic = true,
   },
+
+  -- terminal focused cursor color
+  TermCursor = {
+    bg = "sun",
+  },
+  -- terminal unfocused cursor color
+  TermCursorNC = {
+    bg = "pink",
+  },
 }
+
+--     DiffAdd = {
+--       fg = "#afbdaa",
+--       bg = "#1c2c31",
+--     },
 
 ---@type HLTable
 M.add = {
-  NvimTreeOpenedFolderName = { fg = "green", bold = true },
+  NvimTreeOpenedFolderName = { fg = "#ff00ff", bold = true },
+  MyCustomHi = { bg = "#ff00ff" },
+
+-- -  hi! TermCursor guifg=NONE guibg=yellow gui=NONE cterm=NONE
+-- -  hi! TermCursorNC guifg=yellow guibg=#3c3836 gui=NONE cterm=NONE
+  --
+  -- terminal focused cursor color
+  TermCursor = { bg = "#ff00ff" },
 }
+
+-- vim.api.nvim_set_hl(0, "TermCursor", {
+--   bg = "#9bdead",
+-- })
+-- vim.api.nvim_set_hl(0, "TermCursorNc", {
+--   bg = "pink",
+-- })
 
 return M
